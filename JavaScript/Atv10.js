f78c450a95df304667b26f8e636f63a6d54c696e
Dev-ThiagoLima <Thiago_lima3004@hotmail.com>
2024-01-24 23:29:37 -0300

    update

--- JavaScript/Atv10.js
+++ JavaScript/Atv10.js
@@ -1,1 +1,6 @@
 //10. Crie uma função que receba uma temperatura em Celsius e a converta para Fahrenheit (F = C * 9/5 + 32).
+
+// Exercício 10
+function celsiusParaFahrenheit(celsius) {
+    return celsius * 9/5 + 32;
+  }
