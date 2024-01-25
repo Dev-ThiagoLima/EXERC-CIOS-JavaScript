d1b5a94cd643c085bc2b41dbb366c9d2682aa278
Dev-ThiagoLima <Thiago_lima3004@hotmail.com>
2024-01-24 23:30:34 -0300

    up

--- JavaScript/Atv12.js
+++ JavaScript/Atv12.js
@@ -1,1 +1,10 @@
 //12. Crie uma função que receba um número e retorne true se ele for primo e false se não for.
+
+// Exercício 12
+function ehPrimo(numero) {
+    if (numero < 2) return false;
+    for (let i = 2; i < numero; i++) {
+      if (numero % i === 0) return false;
+    }
+    return true;
+  }
