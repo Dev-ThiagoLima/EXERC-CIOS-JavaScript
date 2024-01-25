62a705d3872f7e1fb43f8e2d4d558cd2f156dc2a
Dev-ThiagoLima <Thiago_lima3004@hotmail.com>
2024-01-24 23:30:11 -0300

    update

--- JavaScript/Atv11.js
+++ JavaScript/Atv11.js
@@ -1,1 +1,6 @@
 //11. Escreva um programa que identifique e imprima os números pares em um array.
+
+// Exercício 11
+function numerosPares(array) {
+    return array.filter(num => num % 2 === 0);
+  }
