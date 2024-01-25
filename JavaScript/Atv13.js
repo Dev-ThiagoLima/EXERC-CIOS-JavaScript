f6e42705f6ae7dd72aeaddc9a9864b5774922545
Dev-ThiagoLima <Thiago_lima3004@hotmail.com>
2024-01-24 23:31:06 -0300

    up

--- JavaScript/Atv13.js
+++ JavaScript/Atv13.js
@@ -1,1 +1,10 @@
 //13. Implemente um programa que calcule o somatório dos números de 1 a 100.
+
+// Exercício 13
+function somatorio() {
+    let soma = 0;
+    for (let i = 1; i <= 100; i++) {
+      soma += i;
+    }
+    return soma;
+  }
